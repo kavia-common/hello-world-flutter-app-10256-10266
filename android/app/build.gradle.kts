fb5d58b4a66df0aa77ae4777702eb793af5e84d3
Kavia AI <production@kavia.ai>
2026-03-02 05:28:45 +0000

    Checkpoint 20 - cm597e1c52

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.froyo.ridekaro.ride_karo"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
